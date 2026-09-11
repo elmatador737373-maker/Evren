eb26c2ea44f109c61b17ab3286160d17e6b236b3
elmatador737373-maker <elmatador737373@gmail.com>
2026-09-11 21:09:01 +0200

    Update evren.py

--- evren.py
+++ evren.py
@@ -78,7 +78,7 @@ bot = commands.Bot(command_prefix="!", intents=intents)
 
 async def my_setup_hook():
     node = wavelink.Node(
-        uri="https://bot-rp-a07c.onrender.com",
+        uri="https://bot-rp-iro0.onrender.com",
         password="youshallnotpass"  # Inserisci la tua password se l'hai cambiata nel file application.yml
     )
 
